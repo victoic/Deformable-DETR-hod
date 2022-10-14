077c7b7818d0dfd5c2665ea895d9d119e14c4228
Victor Lundgren <victor15_toic@hotmail.com>
2022-10-14 19:33:38 -0300

    Update hod.py

--- datasets/hod.py
+++ datasets/hod.py
@@ -77,6 +77,8 @@ class HODataset(VisionDataset):
             self.cache = {}
             self.cache_images()
 
+        self.prepare = ConvertCocoPolysToMask(return_masks)
+
     def __len__(self):
         return len(self.ids)
 
@@ -86,6 +88,7 @@ class HODataset(VisionDataset):
         path = self.coco.loadImgs(img_id)[0]['file_name'].replace('\\', '/')
         print(f'| {self.root} | {path} |')
         img = self.get_image(path)
+        img, target = self.prepare(img, target)
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
@@ -168,6 +171,72 @@ class HODataset(VisionDataset):
     #     target["size"] = torch.as_tensor([int(h), int(w)])
 
     #     return image, target
+
+class ConvertCocoPolysToMask(object):
+    def __init__(self, return_masks=False):
+        self.return_masks = return_masks
+
+    def __call__(self, image, target):
+        w, h = image.size
+
+        image_id = target["image_id"]
+        image_id = torch.tensor([image_id])
+
+        anno = target["annotations"]
+
+        anno = [obj for obj in anno if 'iscrowd' not in obj or obj['iscrowd'] == 0]
+
+        boxes = [obj["bbox"] for obj in anno]
+        # guard against no boxes via resizing
+        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
+        boxes[:, 2:] += boxes[:, :2]
+        boxes[:, 0::2].clamp_(min=0, max=w)
+        boxes[:, 1::2].clamp_(min=0, max=h)
+
+        classes = [obj["category_id"] for obj in anno]
+        classes = torch.tensor(classes, dtype=torch.int64)
+
+        if self.return_masks:
+            segmentations = [obj["segmentation"] for obj in anno]
+            masks = convert_coco_poly_to_mask(segmentations, h, w)
+
+        keypoints = None
+        if anno and "keypoints" in anno[0]:
+            keypoints = [obj["keypoints"] for obj in anno]
+            keypoints = torch.as_tensor(keypoints, dtype=torch.float32)
+            num_keypoints = keypoints.shape[0]
+            if num_keypoints:
+                keypoints = keypoints.view(num_keypoints, -1, 3)
+
+        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
+        boxes = boxes[keep]
+        classes = classes[keep]
+        if self.return_masks:
+            masks = masks[keep]
+        if keypoints is not None:
+            keypoints = keypoints[keep]
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = classes
+        if self.return_masks:
+            target["masks"] = masks
+        target["image_id"] = image_id
+        if keypoints is not None:
+            target["keypoints"] = keypoints
+
+        # for conversion to coco api
+        area = torch.tensor([obj["area"] for obj in anno])
+        iscrowd = torch.tensor([obj["iscrowd"] if "iscrowd" in obj else 0 for obj in anno])
+        target["area"] = area[keep]
+        target["iscrowd"] = iscrowd[keep]
+
+        target["orig_size"] = torch.as_tensor([int(h), int(w)])
+        target["size"] = torch.as_tensor([int(h), int(w)])
+
+        return image, target
+
+
 
 def build(image_set, args):
     root = Path(args.dataset_path)
