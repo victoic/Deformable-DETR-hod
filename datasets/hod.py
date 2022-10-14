16f59339daf59229b8cb92fc3e1dbc78357feebe
Victor Lundgren <victor15_toic@hotmail.com>
2022-10-14 18:34:07 -0300

    Update hod.py

--- datasets/hod.py
+++ datasets/hod.py
@@ -84,7 +84,8 @@ class HODataset(VisionDataset):
         img_id = self.ids[idx]
         target = self.coco.loadAnns(self.coco.getAnnIds(imgIds=img_id))
         path = self.coco.loadImgs(img_id)[0]['file_name']
-        img = self.get_image(path.replace('\\', '/').replace(self.root, ''))
+        root_str = path.replace(str(self.root), '').replace('\\', '/')
+        img = self.get_image(root_str)
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
