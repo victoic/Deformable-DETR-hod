bd739119cc5e74cf24ff20f3a0824277ab0ea2f9
Victor Lundgren <victor15_toic@hotmail.com>
2022-11-25 07:32:59 -0300

    Update run_train.py

--- run_train.py
+++ run_train.py
@@ -68,7 +68,7 @@ class Args:
     self.remove_difficult = True
     self.num_classes = 2
 
-    self.output_dir = '/content/gdrive/Shareddrives/HOD/checkpoints'
+    self.output_dir = 'checkpoints'
     self.device = 'cuda'
     self.seed = 42
     self.resume = ''
