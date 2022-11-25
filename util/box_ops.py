14ae3ae6d6f8a12b060982298044d2c0596fafbe
Victor Lundgren <victor15_toic@hotmail.com>
2022-11-25 18:01:46 -0300

    Update box_ops.py

--- util/box_ops.py
+++ util/box_ops.py
@@ -56,8 +56,9 @@ def generalized_box_iou(boxes1, boxes2):
     """
     # degenerate boxes gives inf / nan results
     # so do an early check
-    #for box in boxes1:
-    #    print('\t', box)
+    for box in boxes1:
+        print('\t', box)
+    
     assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
     assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
     iou, union = box_iou(boxes1, boxes2)
